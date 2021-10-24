a6a841d5b5e84412e9e69d49edc85fe6b57509a0
Richard Stanley <audstanley@gmail.com>
2021-10-24 13:42:11 -0700

    got the parseing almost complete for fiction searches

--- libgen/libgen.go
+++ libgen/libgen.go
@@ -1,13 +1,32 @@
 package libgen
 
 import (
+	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/go-rod/rod"
 )
+
+type Book struct {
+	Author            string `json:"author"`
+	Subtitle          string `json:"subtitle"`
+	Title             string `json:"title"`
+	Language          string `json:"language"`
+	FormatAndSize     string `json:"formatAndSize"`
+	Mirror1           string `json:"mirror1"`
+	LinkToDescription string `json:"linkToDescription"`
+	Description       string `json:"description"`
+}
+
+type WebPageOfBooks struct {
+	Books []Book `json:"books"`
+}
 
 type LibGenSearch struct {
 	NonFiction      *string
@@ -95,8 +114,54 @@ func (search LibGenSearch) fictionSearch() {
 		}
 		// if we are running the software normally (not as a test)
 		if !search.isTestRun() {
-			el := page.MustElement("body > table")
-			search.saveElmentTextToLog(el.MustHTML())
+			err := rod.Try(func() {
+				tbody := page.Timeout(6 * time.Second).MustSearch("tbody")
+				if tbody != nil {
+					pob := WebPageOfBooks{}
+					trs := tbody.MustElements("tr")
+					for _, tr := range trs {
+						tds := tr.MustElements("td")
+						book := Book{}
+						for i, td := range tds {
+							switch i {
+							case 0:
+								book.Author = td.MustText()
+							case 1:
+								book.Subtitle = td.MustText()
+							case 2:
+								book.Title = td.MustText()
+								link := td.MustElement("a")
+								book.LinkToDescription = fmt.Sprint(link.MustProperty("href"))
+							case 3:
+								book.Language = td.MustText()
+							case 4:
+								book.FormatAndSize = td.MustText()
+							case 5:
+								// This is the first mirror URL in the list of td elements
+								link := td.MustElement("a")
+								book.Mirror1 = fmt.Sprint(link.MustProperty("href"))
+							}
+						}
+						pob.Books = append(pob.Books, book)
+						// as := tr.MustElementsX("//a")
+
+						// for i, a := range as {
+						// 	pob.Books[i].Mirror1 = fmt.Sprintln(a.MustHTML())
+						// }
+
+					}
+					search.SaveWebPageOfBooks(pob)
+				}
+
+			})
+			if errors.Is(err, context.DeadlineExceeded) {
+				fmt.Println("Could not find that book in a reasonable amount of time")
+			} else if err != nil {
+				panic(err)
+			}
+			//el := page.MustElement("body > table")
+			//search.saveElmentTextToLog(el.MustHTML())
+
 		}
 	}
 }
@@ -193,4 +258,30 @@ func (search LibGenSearch) GetTitle() string {
 		log.Fatal("The defaults for GetTile are [Fiction, NonFiction, and Scientific] not:", *search.SearchType)
 		return ""
 	}
+}
+
+func (search LibGenSearch) SaveWebPageOfBooks(pob WebPageOfBooks) {
+	for _, b := range pob.Books {
+		// trim up author new line before saving to file:
+		b.Author = strings.Replace(b.Author, "\n", " ", -1)
+	}
+
+	jObj, err := json.MarshalIndent(pob, "", "  ")
+	if err != nil {
+		log.Println(err)
+	}
+
+	err = os.WriteFile(fmt.Sprintf("rod-%s-%s.json", *search.SearchType, search.GetTitle()), []byte("\n"), 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	file, err := os.OpenFile(fmt.Sprintf("rod-%s-%s.json", *search.SearchType, search.GetTitle()), os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Println(err)
+	}
+	defer file.Close()
+	if _, err := file.WriteString(string(jObj)); err != nil {
+		log.Fatal(err)
+	}
+
 }
